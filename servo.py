d6720346644bf4ce9a67ab12415ebbf260b9ea0d
keresztpapa <ifj.solyom.feco15@gmail.com>
2023-04-09 13:39:17 +0200

    fixing crash

--- servo.py
+++ servo.py
@@ -4,46 +4,42 @@ from adafruit_servokit import ServoKit
 # Initialize the servo hat
 global kit
 kit = ServoKit(channels=16)
-
-def move_(angle_from, angle_to, motor_num):
-# Set the initial position of the servo to 0 degrees
-# Change this to the index of your servo
-  kit.servo[motor_num].angle = angle_from
-
-# Define the desired angle and speed
-  speed = 1  # Change this to the desired speed (0 to 1)
-
-# Calculate the pulse width based on the desired angle (150 to 600)
-  pulse_width = int(150 + (angle_to / 180.0) * 450)
-
 # Set the PWM frequency to 50Hz (standard for servos)
-  kit._pca.frequency = 50
+kit._pca.frequency = 50
 
-# Move the servo to the desired angle at the desired speed
-  if angle_from < angle_to:
-    while angle_from < angle_to:
-      print(angle_from)
-      angle_from += speed
-      kit.servo[motor_num].angle = angle_from
-  elif angle_from > angle_to:
-    while angle_from > angle_to:
-      print(angle_from)
-      angle_from -= speed
-      kit.servo[motor_num].angle = angle_from
+def wait(x):
+  if x > 0:
+    time.sleep(x/60*0.1)
+  else:
+    time.sleep(0.3)
+
+def servo_move_and_wait(i, j):
+  kit.servo[i].angle = j
+  wait(j)
 
 def stand():
-  kit.servo[0].angle = 90
-  kit.servo[1].angle = 180
-  kit.servo[2].angle = 180
-  kit.servo[4].angle = 0
-  kit.servo[5].angle = 0
-  kit.servo[6].angle = 0
-  kit.servo[8].angle = 0
-  kit.servo[9].angle = 0
-  kit.servo[10].angle = 0
-  kit.servo[12].angle = 90
-  kit.servo[13].angle = 180
-  kit.servo[14].angle = 180
+  servo_move_and_wait(0, 90)
+  servo_move_and_wait(1, 180)
+  servo_move_and_wait(2, 180)
+  servo_move_and_wait(4, 0)
+  servo_move_and_wait(5, 0)
+  servo_move_and_wait(6, 0)
+  servo_move_and_wait(8, 0)
+  servo_move_and_wait(9, 0)
+  servo_move_and_wait(10, 0)
+  servo_move_and_wait(12, 90)
+  servo_move_and_wait(13, 180)
+  servo_move_and_wait(14, 180)
+
+def walk_forward():
+  servo_move_and_wait(2, 90)
+  servo_move_and_wait(1, 90)
+#  servo_move_and_wait(6, 90)
+#  servo_move_and_wait(5, 90)
+#  servo_move_and_wait(10, 90)
+#  servo_move_and_wait(9, 90)
+#  servo_move_and_wait(14, 90)
+#  servo_move_and_wait(13, 90)
 
 # REVERSED 2-3 bal also
 #horizontal  90 -> 0
@@ -69,4 +65,7 @@ def stand():
 #leg
 #move_(angle_from=180, angle_to=90, motor_num=14)
 
+
+stand()
+walk_forward()
 stand()
