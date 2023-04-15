b4ca8cb3cf01ed89b95a622a46168d6c19156fb8
keresztpapa <ifj.solyom.feco15@gmail.com>
2023-04-15 11:50:45 +0200

    steps

--- servo.py
+++ servo.py
@@ -18,32 +18,85 @@ def servo_move_and_wait(i, j):
   wait(j)
 
 def stand():
-  servo_move_and_wait(0, 90)
+  servo_move_and_wait(0, 60)
   servo_move_and_wait(1, 150)
   servo_move_and_wait(2, 150)
+
   servo_move_and_wait(4, 0)
   servo_move_and_wait(5, 30)
   servo_move_and_wait(6, 30)
+
   servo_move_and_wait(8, 0)
   servo_move_and_wait(9, 30)
   servo_move_and_wait(10, 30)
-  servo_move_and_wait(12, 90)
+
+  servo_move_and_wait(12, 60)
   servo_move_and_wait(13, 150)
   servo_move_and_wait(14, 150)
 
-def walk_forward():
+def stand_up():
 #BAL-FELSO   horizontal :0 ::0->90       vertical: 1 ::180->90       leg: 2 ::180->90
+  servo_move_and_wait(2, 80)
+  servo_move_and_wait(1, 80)
 
-#  servo_move_and_wait(1, 150)
-  pass
+  servo_move_and_wait(6, 100)
+  servo_move_and_wait(5, 100)
 
-#  servo_move_and_wait(1, 50)
-#  servo_move_and_wait(6, 90)
-#  servo_move_and_wait(5, 90)
-#  servo_move_and_wait(10, 90)
-#  servo_move_and_wait(9, 90)
-#  servo_move_and_wait(14, 90)
-#  servo_move_and_wait(13, 90)
+  servo_move_and_wait(10, 100)
+  servo_move_and_wait(9, 100)
+
+  servo_move_and_wait(14, 80)
+  servo_move_and_wait(13, 80)
+
+def right_upper_step():
+  servo_move_and_wait(5, 80)
+  servo_move_and_wait(6, 80)
+  servo_move_and_wait(4, 60)
+
+  servo_move_and_wait(6, 100)
+  servo_move_and_wait(5, 100)
+  servo_move_and_wait(4, 0)
+
+  servo_move_and_wait(5, 80)
+  servo_move_and_wait(6, 80)
+
+
+def right_lower_step():
+  servo_move_and_wait(9, 80)
+  servo_move_and_wait(10, 80)
+  servo_move_and_wait(8, 60)
+
+  servo_move_and_wait(10, 100)
+  servo_move_and_wait(9, 100)
+  servo_move_and_wait(8, 0)
+
+  servo_move_and_wait(10, 80)
+  servo_move_and_wait(9, 80)
+
+def left_upper_step():
+  servo_move_and_wait(1, 100)
+  servo_move_and_wait(2, 100)
+  servo_move_and_wait(0, 120)
+
+  servo_move_and_wait(2, 80)
+  servo_move_and_wait(1, 80)
+  servo_move_and_wait(0, 60)
+
+  servo_move_and_wait(1, 100)
+  servo_move_and_wait(2, 100)
+
+def left_lower_step():
+  servo_move_and_wait(13, 100)
+  servo_move_and_wait(14, 100)
+  servo_move_and_wait(12, 90)
+
+  servo_move_and_wait(14, 80)
+  servo_move_and_wait(13, 80)
+  servo_move_and_wait(12, 150)
+
+  servo_move_and_wait(13, 100)
+  servo_move_and_wait(14, 100)
+
 #NORMAL
 #move_(angle_from=90, angle_to=0, motor_num=4)
 #move_(angle_from=90, angle_to=0, motor_num=5)
@@ -64,5 +117,9 @@ def walk_forward():
 
 
 stand()
-#walk_forward()
+#stand_up()
+left_upper_step()
+left_lower_step()
+right_upper_step()
+right_lower_step()
 #stand()
